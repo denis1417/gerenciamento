a6bc1dea10f328040206b1c6c251beb311dc4440
Denis <denisroberto1417@gmail.com>
2026-02-23 12:34:15 -0300

    Corrigindo conflito no core/urls.py

--- confeitaria/urls.py
+++ confeitaria/urls.py
@@ -1,21 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
 
-    path('admin/', admin.site.urls),
-
-    # URLs do seu app core
-    path('', include('core.urls')),
-
-    # API
-    path('api/', include('core.api_urls')),
+    path('', views.home, name='home'),
 
 ]
-
-# suporte a arquivos media
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
